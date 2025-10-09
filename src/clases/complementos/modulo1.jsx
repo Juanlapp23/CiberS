f3fa6214552f58f033ecee07aa4e4ce61269b8d7
NUYOUWU <dvargasf99@gmail.com>
2025-10-09 19:09:24 +0200

    conexion al boton comenzar modulo

--- src/clases/complementos/modulo1.jsx
+++ src/clases/complementos/modulo1.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import Header from '../components/Header';
-import './ModuleClass.css';
+import Header from '../../components/Header';
+import './estilos/modulo1.css';
 
 function Module1Class1() {
   const [activeTab, setActiveTab] = useState('contenido');
